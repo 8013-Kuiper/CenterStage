6687889bb10ab14bad230f47a3f9f5e14cf18859
rodoggyro <47298960+rodoggyro@users.noreply.github.com>
2023-12-11 16:11:02 -0500

    added winch motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 17.11; // in
+    public static double TRACK_WIDTH = 17.90; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = -0.0256582447677747; //1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = -.0045;
-    public static double kStatic = -.002;
+    public static double kV = -0.0195182447677747; //1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = -0.0039;
+    public static double kStatic = -0.002;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(139);
+    public static double MAX_ANG_VEL = Math.toRadians(179.75008838722556);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
@@ -154,10 +154,10 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: reverse any motors using DcMotor.setDirection()
 
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftRear.setDirection(DcMotorSimple.Direction.FORWARD);                 //setting direction of drive train
-        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);                 //setting direction of drive train
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
 
         List<Integer> lastTrackingEncPositions = new ArrayList<>();
         List<Integer> lastTrackingEncVels = new ArrayList<>();
