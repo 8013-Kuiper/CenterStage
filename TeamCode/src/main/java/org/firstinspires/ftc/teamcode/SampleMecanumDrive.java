8b42d1fb886968d51662e92f53809d624233dc0a
KingTryHard1 <codeshb7@gmail.com>
2023-12-20 17:03:25 -0500

    road runner finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
@@ -84,8 +84,8 @@ import static org.firstinspires.ftc.teamcode.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(1.8, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(.5, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(4, 0, 1e-8);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(4.5, 0, 1e-8);
 
     public static double LATERAL_MULTIPLIER = 1;
 
