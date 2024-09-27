483ce459e797521f1c1c358884e35adbc4cf7d99
KingTryHard1 <codeshb7@gmail.com>
2024-09-27 15:01:54 -0700

    done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LocalizationTest.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.SampleMecanumDrive;
 
@@ -20,6 +21,10 @@ public class LocalizationTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
+        double speed = 0;
+        double curentspeed =0;
+        ElapsedTime timer = new ElapsedTime();
+
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
@@ -27,7 +32,7 @@ public class LocalizationTest extends LinearOpMode {
         while (!isStopRequested()) {
             drive.setWeightedDrivePower(
                     new Pose2d(
-                            gamepad1.left_stick_y,
+                            speed*.5,
                             gamepad1.left_stick_x,
                             gamepad1.right_stick_x
                     )
@@ -35,10 +40,21 @@ public class LocalizationTest extends LinearOpMode {
 
             drive.update();
 
+if (gamepad1.y) {
+    //timer.reset();
+    curentspeed = timer.seconds();
+    speed = 1;
+}
+if(!gamepad1.y)
+    if(timer.seconds()> curentspeed+2){
+        speed = 0;
+    }
+
             Pose2d poseEstimate = drive.getPoseEstimate();
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
+            //telemetry.addData("x", poseEstimate.getX());
+            //telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.addData("distance", drive.leftFront.getCurrentPosition());
             telemetry.update();
         }
     }
